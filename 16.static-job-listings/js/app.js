e286c286db47b5c71f879ef3e98a83f0a32924d8
Aymane1947 <aymaned345@gmail.com>
2021-05-31 19:10:53 +0100

    Add a list of jobs page (From an external JSON file)

---
+++ 16.static-job-listings/js/app.js
@@ -0,0 +1,129 @@
+//TODO: Add cancel filtered elemenets
+const clear = document.getElementById("clear");
+const jobs = document.getElementById("jobs");
+const filteredTags = document.getElementById("filtered-tags");
+const getJobs = async () => {
+  let response = await fetch("js/data.json");
+  let data = await response.json();
+  return data;
+};
+
+getJobs().then((data) => {
+  data.forEach((el) => {
+    addJob(el);
+  });
+  const tags = document.querySelectorAll("#tags");
+  const choosenTags = [];
+  const initialTags = document.querySelectorAll(".initials");
+  tags.forEach((tag) => {
+    tag.addEventListener("click", (e) => {
+      if (e.target.tagName === "SPAN") {
+        filteredTags.style.display = "flex";
+        let html = "";
+        if (!choosenTags.includes(e.target.textContent)) {
+          if (e.target.parentElement.classList.contains("languages")) {
+            html = `
+                    <div class="${e.target.parentElement.className} filtered">
+                      <span>${e.target.textContent}</span>
+                    </div>
+              `;
+            choosenTags.push(e.target.textContent);
+          } else {
+            html = `
+                     <span class="${e.target.className} filtered">${e.target.textContent}</span>
+             `;
+            choosenTags.push(e.target.textContent);
+          }
+
+          filteredTags.firstElementChild.innerHTML += html;
+        }
+
+        initialTags.forEach((job) => {
+          let lngArr = [];
+          let role = job.getElementsByClassName("role")[0].textContent;
+          let level = job.getElementsByClassName("level")[0].textContent;
+          let lng = job.getElementsByClassName("languages")[0].children;
+          lng = Array.from(lng).forEach((lang) =>
+            lngArr.push(lang.textContent)
+          );
+          let existingTags = [role, level, ...lngArr];
+          choosenTags.forEach((el) => {
+            if (!existingTags.includes(el)) {
+              job.style.display = "none";
+            }
+          });
+        });
+      }
+    });
+  });
+
+  // const filtered = document.querySelectorAll(".role.filtered");
+  // filtered.forEach((el) => {
+  //   el.addEventListener("click", (e) => {
+  //     console.log(e.target.textContent);
+  //     choosenTags.splice(choosenTags.indexOf(e.textContent), 1);
+  //     e.target.remove();
+  //   });
+  // });
+
+  clear.addEventListener("click", () => {
+    filteredTags.style.display = "none";
+    choosenTags.length = 0;
+    filteredTags.firstElementChild.innerHTML = "";
+    initialTags.forEach((job) => {
+      console.log(job);
+      job.style.display = "flex";
+    });
+  });
+});
+
+function addJob(el) {
+  let html = `
+    <div class="info">
+      <div class="logo">
+        <img src="${el.logo}" alt="logo-${el.company}" />
+      </div>
+      <div class="content">
+        <div class="info-heading">
+          <h5>${el.company}</h5>
+        <div>`;
+  if (el.new) {
+    html += `<span class="new">NEW!</span>`;
+  }
+  if (el.featured) {
+    html += `<span class="featured">FEATURED</span>`;
+  }
+  html += `  
+        </div>
+       </div>
+        <div class="info-body"><h2>${el.position}</h2></div>
+        <div class="info-footer">
+          <span class="date"> ${el.postedAt}</span>
+          <span class="dot"></span>
+          <span class="type">${el.contract}</span>
+          <span class="dot"></span>
+          <span class="location">${el.location}</span>
+        </div>
+      </div>
+    </div>
+    <div class="tags" id="tags">
+      <span class="role">${el.role}</span>
+      <span class="level">${el.level}</span>
+      <div class="languages">
+      `;
+  el.languages.forEach((language) => {
+    html += `<span>${language}</span>`;
+  });
+  el.tools.forEach((tool) => {
+    html += `<span>${tool}</span>`;
+  });
+  html += `
+      </div>
+    </div>   
+    `;
+  let job = document.createElement("div");
+  job.classList.add("job");
+  job.classList.add("initials");
+  job.innerHTML = html;
+  jobs.append(job);
+}
